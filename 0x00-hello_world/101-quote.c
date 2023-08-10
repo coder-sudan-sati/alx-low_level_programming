57b01d57b5b37f369c63f4385b3877eb248cfe1d
coder-sudan-sati <satialalsadig131@gmail.com>
2023-08-10 00:29:46 -0700

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 
 int main(void)
 {
-  /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-  char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    /**
+    * main - Entry point
+    * Return: Always 1 (Success)
+    */
+  char *message = "and that piece of art is useful\"
+              - Dora Korpar, 2015-10-19\n";
   write(2, message, 59);
   return 1;
 }
