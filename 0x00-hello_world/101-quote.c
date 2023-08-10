e2ac119571c2a395cab5f8586420eab85c5055e6
coder-sudan-sati <satialalsadig131@gmail.com>
2023-08-10 00:16:40 -0700

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+int main(void) {
+  char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(2, message, 50);
+  return 1;
 }
