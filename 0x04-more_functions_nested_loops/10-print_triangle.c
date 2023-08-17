a29e395e4198f0a893cabedd323701f6feedf989
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-08-17 16:46:53 +0200

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of size size
+ * @size: size of the square
+ *
+ * Return: void
+ */
+
+
+void print_square(int size)
+{
+	int i, j,k;
+  
+	if (size <= 0)
+		_putchar('\n');
+   k = size;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size); j++)
+		{ 
+      if(j<k){
+        _putchar('.');     
+      }
+      else
+      {
+        	_putchar('#');
+      }
+		k++
+		}
+		_putchar('\n');
+	}
+
+}
