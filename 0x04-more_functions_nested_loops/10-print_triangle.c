407e28b1cb5dda1412c6901edd606c5c0e7fa1f6
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-08-17 17:53:32 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,37 +1,34 @@
 #include "main.h"
 
 /**
- * print_square - prints a square of size size
- * @size: size of the square
- *
+ * print_triangle - entry point
+ * owned by Bwave/Bright Daniel
+ * Description: Prints diagonals
+ *@size: size of the triangle
  * Return: void
  */
 
-
 void print_triangle(int size)
 {
-	int i, j,k;
-        k = size;
+	int row, hashes, spaces;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < (size); j++)
-		{ 
-                   if(j<k-1){
-                      _putchar(' ');     
-                   }
-                   else
-                      {
-        	        _putchar('#');
-                      }
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-		k--;
-
 }
