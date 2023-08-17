7fe48b1b73e11593929702c61f2a00eba114197b
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-08-17 17:12:34 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,19 +13,23 @@ void print_square(int size)
 	int i, j,k;
         k = size;
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
+	else
+	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < (size); j++)
 		{ 
-      if(j<k){
-        _putchar('.');     
-      }
-      else
-      {
-        	_putchar('#');
-      }
-		k++
+                   if(j<k){
+                      _putchar(' ');     
+                   }
+                   else
+                      {
+        	        _putchar('#');
+                      }
+		k++;
 		}
 		_putchar('\n');
 	}
