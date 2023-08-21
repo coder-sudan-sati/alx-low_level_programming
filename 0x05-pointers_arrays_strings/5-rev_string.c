8b49fcae221a2dff3169c6689f24f99c6f862682
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-08-21 17:51:01 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,8 +7,13 @@
   *
   * Return: void
   */
-void rev_string(char *s)
-{
-	strrev(*s);
-	_putchar(s);
+void rev_string(char *s) {
+    int len = strlen(s);
+    char temp;
+    for(int i=0; i<len/2; i++) {
+        temp = s[i];
+        s[i] = s[len-i-1];
+        s[len-i-1] = temp;
+    }
 }
+
