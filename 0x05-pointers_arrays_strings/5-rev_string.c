72c8e3649ce12011cb52aaa7d0616ffbf864d0cf
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-08-21 17:31:29 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,33 @@
 #include "main.h"
 
-/*
- *   char *ptr = reverse(STR);
- *
- * Reverse the characters of STR[] in place, and return STR.
- */
-char *reverse(char *s) {
-  char ch, *p, *q;
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, aux;
 
-  /* Point q at the last character in s[], or at &s[0] if s is empty. */
-  for (q = s; *q != '\0'; ++q)
-    ;
-  if (q > s)
-    --q;
-  
-  /* Simultaneously sweep p from front to back and q from back to front,
-   * swapping characters at p and q, until the pointers meet. */
-  for (p = s; p < q; ++p, --q) {
-    ch = *p;
-    *p = *q;
-    *q = ch;
-  }
+	a = s;
 
-  return s;
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
 }
