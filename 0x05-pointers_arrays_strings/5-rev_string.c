feda5d5973f5b66cc68db65787693b1ccf482d5b
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-08-21 17:55:44 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,8 @@ void rev_string(char *s)
    char *start = s;
    char *end = s + len - 1;
    
-   while (start < end) {
+   while (start < end)
+    {
       char temp = *start;
       *start = *end;
       *end = temp;
