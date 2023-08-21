804b240e0f1dcd28678f4920012efce08b5e9ba9
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-08-21 17:37:08 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "string.h"
 
 /**
   * rev_string - Reverses a string
@@ -8,26 +9,6 @@
   */
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
-
-	a = s;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
+	strrev(*s);
+	_putchar(s);
 }
