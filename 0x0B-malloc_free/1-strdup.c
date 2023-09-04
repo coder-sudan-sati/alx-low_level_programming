9f12b87278fc11d010e73f3f8d9fdc0b8117c63c
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-09-04 09:02:51 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,50 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-  
- /** 
-  * string_nconcat - concatenate 2 strings, only n bytes of s2 
-  * @s1: string 1 
-  * @s2: string 2 
-  * @n: bytes to include of s2 
-  * Return: NULL if fail, else pointer to malloc memory 
-  */ 
-  
- char *string_nconcat(char *s1, char *s2, unsigned int n) 
- { 
-         char *p; 
-         int strlen1, i, c; 
-  
-         if (s1 == NULL) 
-                 s1 = ""; 
-         if (s2 == NULL) 
-                 s2 = ""; 
-  
-         strlen1 = (unsigned int)_strlen(s1); 
-         p = malloc((strlen1 + n + 1) * sizeof(char)); 
-         if (p == NULL) 
-                 return (NULL); 
-         for (i = 0, c = 0; i < (strlen1 + n); i++) 
-         { 
-                 if (i < strlen1) 
-                         p[i] = s1[i]; 
-                 else 
-                         p[i] = s2[c++]; 
-         } 
-         p[i] = '\0'; 
-  
-         return (p); 
- } 
-  
- /** 
-  * _strlen - find length of string 
-  * @s: string 
-  * Return: length of string 
-  */ 
-  
- int _strlen(char *s) 
- { 
-         int i; 
-  
-         for (i = 0; s[i] != '\0'; i++) 
-                 ; 
-         return (i); 
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicate a string using malloc
+ * @str: string to duplicate
+ * Return: Pointer to a the new duped string
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int i, c;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	a = malloc(i * sizeof(*a) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0; c < i; c++)
+		a[c] = str[c];
+	a[c] = '\0';
+
+	return (a);
+}
