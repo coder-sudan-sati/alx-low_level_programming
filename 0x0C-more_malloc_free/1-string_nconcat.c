e78eb59d1091cf45a6b3f60045bc00f4e8f3a7dd
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-09-07 11:22:07 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,44 @@
-#include "main.h"
-
-/**
-*string_nconcat - Concatenates two strings using at
-*most an inputted number of bytes.
-*@s1: The first string.
-*@s2: The second string.
-*@n: The maximum number of bytes of s2 to concatenate to s1.
-*
-*Return: If the function fails - NULL.
-*Otherwise - a pointer to the concatenated space in memory.
-*/
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *concat;
-	unsigned int len = n, index;
-
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	for (index = 0; s1[index]; index++)
-		len++;
-
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
-		return (NULL);
-
-	len = 0;
-
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-
-	return (concat);
+#include <stdlib.h> 
+  
+ /** 
+  * string_nconcat - Concatenate two strings using n amount of s2 
+  * @s1: First string 
+  * @s2: String to add to end of s1 
+  * @n: Amount of s2 to add to s1 
+  * 
+  * Return: pointer to new area in memory, NULL if it fails 
+  */ 
+ char *string_nconcat(char *s1, char *s2, unsigned int n) 
+ { 
+         char *nstr, *empt; 
+         unsigned int i, len, j; 
+         unsigned int size; 
+  
+         len = 0; 
+         empt = ""; 
+         if (s1 == NULL) 
+                 s1 = empt; 
+         if (s2 == NULL) 
+                 s2 = empt; 
+         while (s1[len] != '\0') 
+                 len++; 
+         size = (len + n) * sizeof(*nstr); 
+         nstr = malloc(size + 1); 
+         if (nstr == NULL) 
+                 return (NULL); 
+         i = 0; 
+         while (i < size && s1[i] != '\0') 
+         { 
+                 nstr[i] = s1[i]; 
+                 i++; 
+         } 
+         j = 0; 
+         while (i < size && s2[j] != '\0') 
+         { 
+                 nstr[i] = s2[j]; 
+                 i++; 
+                 j++; 
+         } 
+         nstr[i] = '\0'; 
+         return (nstr); 
 }
