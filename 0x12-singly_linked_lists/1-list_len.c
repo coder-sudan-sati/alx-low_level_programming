d256cb2037ae162986f0aabf297004f28c527ddc
coder-sudan-sati <43772586+coder-sudan-sati@users.noreply.github.com>
2023-09-22 02:40:57 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,22 @@
 #include "lists.h"
-/**
-  * listint_len - gets the number of nodes in a list
-  * @h: singly linked list to print
-  * Return: number of nodes in the list
-  */
-size_t listint_len(const listint_t *h)
-{
-	size_t i;
 
-	for (i = 0; h; i++)
-		h = h->next;
-	return (i);
+/**
+ * list_len - Returns the number of elements in a linked `list_t` list
+ * @h: head of linked list
+ * Return: number of elements in list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *current;
+	size_t c;
+
+	c = 0;
+	current = h;
+	while (current != NULL)
+	{
+		current = current->next;
+		c++;
+	}
+
+	return (c);
 }
